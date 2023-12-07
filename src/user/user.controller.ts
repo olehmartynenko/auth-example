c8f465bdacb1c3cfa44fe1001dca29098cb09202
olehmartynenko <martynenkooleg77@gmail.com>
2023-12-07 19:00:55 +0200

    Implemented getting user info

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto, UserDto } from './dto';
 import { AuthGuard } from 'src/auth/guards';
+import { Request } from 'express';
 
 @UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
   @Get('me')
-  me(): Promise<any> {
-    return this.userService.me();
+  me(@Req() req: Request): Promise<UserDto> {
+    return this.userService.me(req.headers.authorization);
   }
 
   @Patch(':id')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto, UserDto } from './dto';
+import { AuthException } from '../auth/exceptions/auth.exceptions';
+import { jwtDecode } from 'jwt-decode';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  async me(): Promise<UserDto> {
-    return {
-      id: 1,
-      email: 'test@test.com',
-      name: 'test',
-    };
+  constructor(private prisma: PrismaService) {}
+
+  async me(auth): Promise<UserDto> {
+    const token = auth.split(' ')[1];
+    const payload = jwtDecode(token);
+    const id = Number(payload.sub);
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new AuthException('User is not found');
+    }
+
+    delete user.password;
+    return user;
   }
 
   async editUser(userId: number, data: UpdateUserDto): Promise<UserDto> {
