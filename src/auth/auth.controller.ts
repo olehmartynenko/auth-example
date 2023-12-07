85f5950ea56da80ddc7aebd4ae571b485b256b9a
olehmartynenko <martynenkooleg77@gmail.com>
2023-12-07 19:39:21 +0200

    Login should return status 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { Response } from 'express';
@@ -7,7 +14,7 @@ import { CreateUserDto } from 'src/user/dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   async login(
     @Body() credentials: AuthDto,
