79ab0899518878f8df8717b64ebb0df3bbc3fae9
olehmartynenko <martynenkooleg77@gmail.com>
2023-12-07 18:26:55 +0200

    Implemented login with jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { UserDto } from 'src/user/dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() credentials: AuthDto): Promise<UserDto> {
-    return this.authService.login(credentials);
+  async login(
+    @Body() credentials: AuthDto,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const token = await this.authService.login(credentials);
+    return res.set({ 'x-access-token': token }).send({ token });
   }
 
   @Post('signup')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,17 @@ import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { CreateUserDto, UserDto } from 'src/user/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  async login(credentials: AuthDto): Promise<UserDto> {
+  async login(credentials: AuthDto): Promise<string> {
     const user = await this.prisma.user.findUnique({
       where: { email: credentials.email },
     });
@@ -23,9 +29,7 @@ export class AuthService {
       throw new ForbiddenException('Invalid password');
     }
 
-    delete user.password;
-
-    return user;
+    return this.signToken(user);
   }
 
   async signUp(data: CreateUserDto): Promise<UserDto> {
@@ -46,5 +50,13 @@ export class AuthService {
         throw new ForbiddenException('User with this email already exists');
       }
     }
+  }
+
+  signToken(user: UserDto): Promise<string> {
+    const payload = { email: user.email, sub: user.id };
+    return this.jwt.signAsync(payload, {
+      expiresIn: this.config.get('JWT_ACCESS_EXPIRATION'),
+      secret: this.config.get('JWT_SECRET'),
+    });
   }
 }
