9b7c523ddfff3126d64e7b7bcc58c3d3ad95a209
olehmartynenko <martynenkooleg77@gmail.com>
2023-12-07 17:17:28 +0200

    Refactored

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AuthController, UserController],
-  providers: [AuthService, UserService],
+  imports: [AuthModule, UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
